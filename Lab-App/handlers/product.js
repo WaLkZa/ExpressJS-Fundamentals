2bd3c7c90943205e5d8ed79fab7066028a1e3932
WaLkZa <walkzi@yandex.com>
2018-06-13 00:52:45 +0300

    Part III – Defining Database Models and Relations

--- Lab-App/handlers/product.js
+++ Lab-App/handlers/product.js
@@ -1,10 +1,12 @@
 const url = require('url');
-const database = require('../config/database');
+//const database = require('../config/database');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
 const multiparty = require('multiparty');
 const shortid = require('shortid');
+const Product = require('../models/Product')
+const Category = require('../models/Category')
 
 module.exports = (req, res) => {
     req.pathname = req.pathname || url.parse(req.url).pathname
@@ -24,12 +26,23 @@ module.exports = (req, res) => {
                 return
             }
 
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
+            Category.find().then((categories) => {
+                let replacement = '<select class="input-field" name="category">'
+                for (let category of categories) {
+                    replacement += `$<option value="${category._id}">${category.name}</option>`
+                }
 
-            res.write(data)
-            res.end()
+                replacement += '</select>'
+
+                let html = data.toString().replace('{categories}', replacement)
+
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                })
+
+                res.write(html)
+                res.end()
+            })
         })
     } else if (req.pathname === '/product/add' && req.method === 'POST') {
         let form = new multiparty.Form()
@@ -51,7 +64,9 @@ module.exports = (req, res) => {
 
                     product.image = filePath
                     fs.writeFile(
-                        `.${filePath}`, dataString, { encoding: 'ascii' }, (err) => {
+                        `.${filePath}`, dataString, {
+                            encoding: 'ascii'
+                        }, (err) => {
                             if (err) {
                                 console.log(err);
                                 return
@@ -73,12 +88,18 @@ module.exports = (req, res) => {
         })
 
         form.on('close', () => {
-            database.products.add(product)
-            res.writeHead(302, {
-                Location: '/'
+            Product.create(product).then((insertedProduct) => {
+                Category.findById(product.category).then(category => {
+                    category.products.push(insertedProduct._id)
+                    category.save()
+                })
+                
+                res.writeHead(302, {
+                    Location: '/'
+                })
+
+                res.end()
             })
-            
-            res.end()
         })
 
         form.parse(req)
